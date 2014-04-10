07c9ea74c3a556cc3c4ca62efafb646447480781
zakuro9715 <zakruo9715@yahoo.co.jp>
2014-04-11 00:08:59 +0900

    Add IMailBoxWriter

---
+++ AkaneMail/AkaneMail/IMailBoxWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace AkaneMail
+{
+  public interface IMailBoxWriter
+  {
+    void Write(MailBox mailBox, Stream stream);
+  }
+}
